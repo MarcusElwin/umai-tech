a91a48694cf691cf323e41356134fb34ca909d90
Marcus Elwin <marcus.elwin@kustom.co>
2025-07-24 23:05:27 +0200

    fix: deps

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
-["src/content/blog/prompt-eng-ner.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fprompt-eng-ner.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/unpopular-opinion-hard-good-ds.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Funpopular-opinion-hard-good-ds.mdx&astroContentModuleFlag=true")],
-["src/content/blog/ner-dspy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fner-dspy.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/ner-dspy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fner-dspy.mdx&astroContentModuleFlag=true")],
+["src/content/blog/prompt-eng-ner.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fprompt-eng-ner.mdx&astroContentModuleFlag=true")]]);
 		
